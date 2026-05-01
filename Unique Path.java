ace7b3a3a6638dd336e200331d94cf99c3a21fa8
Priyanka-N2781 <priyanka.n2781@gmail.com>
2026-05-01 14:24:20 +0530

    Implement unique paths calculation in Unique Path.java

---
+++ Unique Path.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] dp = new int[n][m];
+        for(int j=0;j<m;j++){
+            dp[0][j] = 1;
+        }
+        for(int i=0;i<n;i++){
+            dp[i][0] = 1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        System.out.println(dp[n-1][m-1]);
+    }
+}
