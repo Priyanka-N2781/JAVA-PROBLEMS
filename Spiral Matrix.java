25b7c909e3ffb26523cd670f7656b7cea6024a96
Priyanka-N2781 <priyanka.n2781@gmail.com>
2026-05-01 14:23:26 +0530

    Implement spiral matrix traversal in Java

---
+++ Spiral Matrix.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] mat = new int[rows][cols];
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        int top = 0;
+        int bottom = rows - 1;
+        int left = 0;
+        int right = cols - 1;
+
+        while(top <= bottom && left <= right) {
+
+            for(int i = left; i <= right; i++) {
+                System.out.print(mat[top][i] + " ");
+            }
+            top++;
+
+            for(int i = top; i <= bottom; i++) {
+                System.out.print(mat[i][right] + " ");
+            }
+            right--;
+
+            if(top <= bottom) {
+                for(int i = right; i >= left; i--) {
+                    System.out.print(mat[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            if(left <= right) {
+                for(int i = bottom; i >= top; i--) {
+                    System.out.print(mat[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
+}
