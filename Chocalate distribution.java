8d574226ee095785f0af276612fcdac4335e7e34
Priyanka N <priyanka.n2781@gmail.com>
2026-06-30 17:46:27 +0530

    Create Chocalate distribution.java

---
+++ Chocalate distribution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int m = sc.nextInt();
+
+        Arrays.sort(arr);
+
+        int minDiff = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= n - m; i++) {
+            int diff = arr[i + m - 1] - arr[i];
+            minDiff = Math.min(minDiff, diff);
+        }
+
+        System.out.println(minDiff);
+    }
+}
